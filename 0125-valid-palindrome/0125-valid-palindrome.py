44ca7a611f384548f511c31716973f651020fdd2
SWATISUCHARITA APATTA <148532916+swatisucharita-04@users.noreply.github.com>
2026-03-19 19:58:27 +0530

    Time: 236 ms (12.85%), Space: 13 MB (60.36%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        
+        cleaned = ""
+
+        for char in s:
+            if char.isalnum():
+                cleaned += char.lower()
+
+        return cleaned == cleaned[::-1]
