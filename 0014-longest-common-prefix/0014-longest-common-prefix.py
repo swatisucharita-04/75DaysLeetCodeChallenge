dc220ac82027e8ed99e1e6a26dbc88b4445d3511
SWATISUCHARITA APATTA <148532916+swatisucharita-04@users.noreply.github.com>
2026-04-26 10:17:08 +0530

    Time: 1 ms (38.83%), Space: 12.5 MB (36.27%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        
+        if not strs:
+            return ""
+        
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            
+            for s in strs[1:]:
+                # if index out of range OR char mismatch
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]
+        
+        return strs[0]
