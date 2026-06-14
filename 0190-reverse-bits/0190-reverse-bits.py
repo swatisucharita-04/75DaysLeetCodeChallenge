6d8984bc083c6a5fe399693e79b8ec0fe0ff0f1d
SWATISUCHARITA APATTA <148532916+swatisucharita-04@users.noreply.github.com>
2026-06-14 12:50:22 +0530

    Time: 24 ms (12.82%), Space: 12.3 MB (52.21%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseBits(self, n):
+        
+        result = 0
+
+        for _ in range(32):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+
+        return result
